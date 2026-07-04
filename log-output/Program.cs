3fd22582f03de8b7f9c896bb5f5078da48cec7b0
JJnne <janneheikkila93@gmail.com>
2026-07-04 17:24:50 +0200

    1.7: log-output status endpoint and Ingress

--- log-output/Program.cs
+++ log-output/Program.cs
@@ -1,8 +1,23 @@
-﻿string randomString = Guid.NewGuid().ToString();
+string randomString = Guid.NewGuid().ToString();
 
-while (true)
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:3000");
+var app = builder.Build();
+
+app.MapGet("/", () =>
 {
     string timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffZ");
-    Console.WriteLine($"{timestamp}: {randomString}");
-    Thread.Sleep(TimeSpan.FromSeconds(5));
-}
+    return $"{timestamp}: {randomString}";
+});
+
+_ = Task.Run(async () =>
+{
+    while (true)
+    {
+        string timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffZ");
+        Console.WriteLine($"{timestamp}: {randomString}");
+        await Task.Delay(TimeSpan.FromSeconds(5));
+    }
+});
+
+app.Run();
