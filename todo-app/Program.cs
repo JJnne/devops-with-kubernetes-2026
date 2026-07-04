a9b6361b6cd8a0d04504b4414ec6afdcd33901c9
JJnne <janneheikkila93@gmail.com>
2026-07-04 02:10:01 +0200

    todo app v1

---
+++ todo-app/Program.cs
@@ -0,0 +1,13 @@
+﻿string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Server started in port " + port);
+
+app.Lifetime.ApplicationStarted.Register(() =>
+    Console.WriteLine($"Server started in port {port}"));
+
+app.Run();
